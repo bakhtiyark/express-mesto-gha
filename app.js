6894cbd9a646ba91018a62c2b9056e7775b1239d
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-09-24 22:29:56 +0500

    sk

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
+const { cardRouter } = require('./routes/cards');
+
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+
+app.listen(PORT);
