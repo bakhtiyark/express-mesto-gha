c569b43bc35862ac85bed111f58fa46edecf6402
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-09-27 18:17:54 +0500

    basic models

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Жак-Ив Кусто',
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Исследователь',
+  },
+  avatar: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+
+module.exports = mongoose.model('user', userSchema);
