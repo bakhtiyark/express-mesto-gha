afb57c14eb5a4e2347f63d065eb1af2b8dac609c
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-09-28 16:34:22 +0500

    Карты

--- app.js
+++ app.js
@@ -7,5 +7,14 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-
+app.use((req, res, next) => {
+    req.user = {
+      _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    };
+  
+    next();
+  });
+  module.exports.createCard = (req, res) => {
+    console.log(req.user._id); // _id станет доступен
+  };
 app.listen(PORT);
---
+++ controllers/cards.js
@@ -0,0 +1,91 @@
+//Импорт модели
+const Card = require('../models/card');
+
+//Ошибки
+const invalidData = {
+    "code": 400,
+    "message": "переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля;"
+}
+
+const dataNotFound = {
+    "code": 404,
+    "message": "Карточка или пользователь не найден."
+}
+const defaultError = {
+    "code": 500,
+    "message": "Общая ошибка"
+}
+// Создание карты
+const createCard = (req, res, next) => {
+    const { name, link } = req.body;
+    const owner = req.user._id;
+
+    Card.create({ name, link, owner })
+        .then((card) => res.status(201).send(card))
+        .catch((err) => {
+            if (err.status === invalidData.code) {
+                return next(invalidData.message);
+            } else {
+                return next(err);
+            }
+        });
+};
+
+// Получить все карты
+const getCards = (res, next) => {
+    Card.find({})
+        .then((cards) => res.status(200).send(cards))
+        .catch(next);
+};
+
+// Удалить карточку
+const deleteCard = (req, res, next) => {
+    const { cardId } = req.params;
+
+    Card.findById(cardId)
+        .then((card) => {
+            if (!card) {
+                throw dataNotFound.message;
+            }
+            return Card.findByIdAndRemove(cardId)
+                .then(() => res.status(200).send({ message: 'Карточка удалена' }))
+                .catch(next);
+        })
+};
+
+// Поставить лайк карточке
+const likeCard = (req, res) => {
+    Card.findByIdAndUpdate(
+        req.params.cardId,
+        { $addToSet: { likes: req.user._id } },
+        { new: true },
+    ).then((card) => {
+        if (!card) {
+            throw dataNotFound.message;
+        } else {
+            res.status(200).send(card);
+        }
+
+    })
+};
+//Удаления лайка с карты
+const removeLike = (req, res) => {
+    Card.findByIdAndUpdate(
+        req.params.cardId,
+        { $pull: { likes: req.user._id } },
+        { new: true },
+    ).then((card) => {
+        if (!card) {
+            throw dataNotFound.message;
+        }
+        res.status(200).send(card);
+    })
+};
+
+module.exports = {
+    createCard,
+    getCards,
+    deleteCard,
+    likeCard,
+    removeLike,
+};
