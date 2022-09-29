43fb00908812da6788fb54cd0d47a354386316f2
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-09-29 19:07:55 +0500

    minor

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const { PORT = 3000 } = process.env;
 const { cardRouter } = require('./routes/cards');
 
@@ -8,13 +9,14 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
-    req.user = {
-      _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
-    };
-  
-    next();
-  });
-  module.exports.createCard = (req, res) => {
-    console.log(req.user._id); // _id станет доступен
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
+
+  next();
+});
+module.exports.createCard = (req, res) => {
+  console.log(req.user._id); // _id станет доступен
+};
+
 app.listen(PORT);
