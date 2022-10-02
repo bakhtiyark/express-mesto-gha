6fb479fc98243ab5dbb41cbb289791723d26b184
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-10-02 21:54:29 +0500

    routing

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(express.json())
+
 app.use((req, res, next) => {
   req.user = {
     _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
@@ -20,6 +22,10 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.use("/users", userRouter)
+app.use("/cards", cardRouter)
+
 module.exports.createCard = (req, res) => {
   console.log(req.user._id); // _id станет доступен
 };
