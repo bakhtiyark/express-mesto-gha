68c230a94fbae46b2a18081f9dd4ddd67db9b631
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-10-07 00:40:37 +0500

    test 0.18

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const { NotFound, ValidationError } = require("../errors/errors")
 
 const createUser = (req, res) => {
     const { name, about, avatar } = req.body;
--- models/user.js
+++ models/user.js
@@ -18,17 +18,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
+  }
 });
 
 
