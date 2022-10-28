139b127825915f33aef9fd919f2177f9305a8b9b
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-10-28 23:27:23 +0500

    1

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/crash-test', () => {
 app.use(requestLogger);
 app.use(errorLogger);
 
-app.post('/sign-in', celebrate({
+app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
@@ -54,7 +54,7 @@ app.post('/sign-in', celebrate({
 }), login);
 
 // reg
-app.post('/sign-up', celebrate({
+app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
