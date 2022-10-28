c796cdebd6ca437774c4db77feedad87921d5649
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-10-28 20:30:22 +0500

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,14 +11,18 @@ const cors = (req, res, next) => {
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Headers', origin);
-  }
-
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', DEFAULT_ALLOWED_METHODS);
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Credentials', true);
     return res.end();
+  }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   return next();
 };
